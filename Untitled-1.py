473a2ab7327b2224108c78f75106da49b51ba23b
Sathwik Tuduru <sathwiktuduru@gmail.com>
2025-01-23 14:15:09 +0530

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+# first_name=input("Enter First Name: ")
+# last_name=input("Enter Last Name: ")
+# print(f"Surname {last_name}, Name {first_name}")
+
+# f=input("Enter your name: ")
+# address=input("Enter your address")
+# print("Name: "+f+"\nAddress: "+address)
+
